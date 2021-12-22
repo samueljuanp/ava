ebb9ca4b23cebd4326267646865ee8ec17f377dd
Samuel Juan Prasetya <sprasetya@investcloud.com>
2021-12-22 15:03:31 +0800

    Compute factor model

--- ava/commands/quant.py
+++ ava/commands/quant.py
@@ -3,28 +3,44 @@ from ava.services.quant import Quantitative
 
 
 class Context:
-    def __init__(self, ticker):
+    def __init__(self, ticker, window):
         self.ticker = ticker
-        self.compute = Quantitative()
+        self.window = window
+        self.quant = Quantitative(ticker, window)
 
 
 @click.group()
-@click.option('--ticker', type=str, default='AAPL', help="Yahoo Finance ticker")
+@click.option('--ticker', type=str, default='SPY', help="Yahoo Finance ticker")
+@click.option('--window', type=int, default=3650, help="Lookback period in days")
 @click.pass_context
-def cli(ctx, ticker):
+def cli(ctx, ticker, window):
     """Quantitative Analysis"""
-    ctx.obj = Context(ticker)
+    ctx.obj = Context(ticker, window)
 
 
 @cli.command()
 @click.pass_context
 def alpha(ctx):
-    """Alpha from Fama French 4-Factor Model"""
-    click.echo(ctx.obj.ticker)
+    """Alpha from Carhart 4-Factor Model"""
+    click.echo(ctx.obj.quant.get_alpha())
 
 
 @cli.command()
 @click.pass_context
 def beta(ctx):
-    """Beta from Fama French 4-Factor Model"""
-    click.echo(ctx.obj.ticker)
+    """Beta from Carhart 4-Factor Model"""
+    click.echo(ctx.obj.quant.get_beta())
+
+
+@cli.command()
+@click.pass_context
+def regress(ctx):
+    """Regression Summary Table"""
+    click.echo(ctx.obj.quant.get_regression_summary())
+
+
+@cli.command()
+@click.pass_context
+def profile(ctx):
+    """Risk-Adjusted Return Profile"""
+    click.echo(ctx.obj.quant.get_risk_return_profile())
--- ava/services/quant.py
+++ ava/services/quant.py
@@ -1,2 +1,93 @@
+import numpy as np
+import pandas as pd
+import datetime as dt
+import pandas_datareader as wb
+import getFamaFrenchFactors as gff
+import statsmodels.formula.api as smf
+
+
 class Quantitative:
-    pass
+    def __init__(self, ticker, window):
+        self.ticker = ticker
+        self.end = dt.datetime.today()
+        self.start = self.end - dt.timedelta(days=window)
+        self.price = None
+        self.factor = None
+
+    def _compute_regression(self):
+
+        # fetch price data from Yahoo Finance
+        price = wb.DataReader(self.ticker, 'yahoo', self.start, self.end)['Adj Close']
+        price.name = self.ticker
+        monthly_return = price.resample('M').last().pct_change().dropna()
+
+        # fetch factor data from getFamaFrenchFactors
+        factor = gff.carhart4Factor(frequency='m')
+        factor.rename(columns={'date_ff_factors': 'Date'}, inplace=True)
+        factor.rename(columns={'Mkt-RF': 'MKT'}, inplace=True)
+        factor.set_index('Date', inplace=True)
+
+        # assign to attributes
+        self.price = price
+        self.factor = factor
+
+        # compute regression
+        ff_data = factor.merge(monthly_return, on='Date')
+        ff_model = smf.ols(formula=f"{self.ticker} ~ MKT + SMB + HML + MOM", data=ff_data)
+        result = ff_model.fit()
+
+        return result
+
+    def get_alpha(self):
+        result = self._compute_regression()
+        alpha = result.params['Intercept']
+        print(f"Alpha of {self.ticker}: {alpha:.3f}")
+
+    def get_beta(self):
+        result = self._compute_regression()
+        beta = result.params['MKT']
+        print(f"Beta of {self.ticker}: {beta:.3f}")
+
+    def get_regression_summary(self):
+        result = self._compute_regression()
+        print()
+        print(result.summary())
+
+    def _compute_expected_return(self):
+        result = self._compute_regression()
+        param_pvalue = pd.concat([result.params, result.pvalues], axis=1)
+        param_pvalue.columns = ['Param', 'P-Value']
+
+        # get coefficients with significant p-value
+        significant = param_pvalue[param_pvalue['P-Value'] <= 0.05]
+
+        # compute expected return
+        if 'Intercept' in significant.index:
+            alpha = significant.loc['Intercept', 'Param']
+            significant = significant.drop('Intercept')
+            exp_month = (self.factor[significant.index].mean() * significant['Param']).sum() + alpha
+            exp_annual = exp_month * 12
+        else:
+            exp_month = (self.factor[significant.index].mean() * significant['Param']).sum()
+            exp_annual = exp_month * 12
+
+        return exp_annual
+
+    def _compute_hist_volatility(self):
+        return self.price.pct_change().std(skipna=True) * np.sqrt(252)
+
+    def get_risk_return_profile(self):
+        exp_annual = self._compute_expected_return()
+        vol = self._compute_hist_volatility()
+        sharpe = exp_annual / vol
+
+        # log messages
+        print(f"\nRisk & Return Profile of {self.ticker}")
+        print('=' * (25 + len(self.ticker)))
+        print(f"Expected annual return: {(exp_annual * 100):.2f}%")
+        print(f"Historical volatility: {(vol * 100):.2f}%")
+        print(f"Risk-adjusted return: {sharpe:.3f}")
+
+
+
+
